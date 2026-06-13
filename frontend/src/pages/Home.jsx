60316ec2344c52d3466ed1e0ead31f1008004095
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 00:55:00 -0700

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 function Home() {
   return (
     <div>
+      <Navbar />
+
       <h1>CampusKart</h1>
       <p>Daily essentials delivered fast.</p>
+
+      <Footer />
     </div>
   );
 }
