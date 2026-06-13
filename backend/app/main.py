b302688306e78a24aeaee05ad58fe620f34e892a
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 01:15:17 -0700

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,1 +1,15 @@
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "CampusKart backend is running"}
+
+@app.get("/products")
+def get_products():
+    return [
+        {"id": 1, "name": "Maggi", "price": 15},
+        {"id": 2, "name": "Milk", "price": 60},
+        {"id": 3, "name": "Bread", "price": 40},
+    ]
