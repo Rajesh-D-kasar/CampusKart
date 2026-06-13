b37529d5b161bbe3dabf89e93157443bc6985fc2
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 00:57:50 -0700

    Create Login.jsx

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,15 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+
+      <input type="email" placeholder="Enter email" />
+      <br />
+      <input type="password" placeholder="Enter password" />
+      <br />
+      <button>Login</button>
+    </div>
+  );
+}
+
+export default Login;
