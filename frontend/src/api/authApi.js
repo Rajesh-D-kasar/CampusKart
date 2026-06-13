e80942f591e746d66ca1605ef6b171a17559b542
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 01:08:25 -0700

    Update authApi.js

--- frontend/src/api/authApi.js
+++ frontend/src/api/authApi.js
@@ -1,1 +1,11 @@
+import API from "./axiosConfig";
 
+export const registerUser = async (userData) => {
+  const response = await API.post("/register", userData);
+  return response.data;
+};
+
+export const loginUser = async (loginData) => {
+  const response = await API.post("/login", loginData);
+  return response.data;
+};
