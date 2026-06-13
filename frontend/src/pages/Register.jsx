d023b5404206bc3743892d873669087b985c2517
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 00:58:13 -0700

    Create Register.jsx

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,17 @@
+function Register() {
+  return (
+    <div>
+      <h2>Register</h2>
+
+      <input type="text" placeholder="Enter name" />
+      <br />
+      <input type="email" placeholder="Enter email" />
+      <br />
+      <input type="password" placeholder="Enter password" />
+      <br />
+      <button>Create Account</button>
+    </div>
+  );
+}
+
+export default Register;
