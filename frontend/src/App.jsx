686c29b5025d048c215c57793f15af6554235bf2
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 01:10:03 -0700

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,15 @@
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Cart from "./pages/Cart";
 
+function App() {
+  return (
+    <div>
+      <Home />
+      <Products />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
