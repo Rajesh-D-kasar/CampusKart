2aa4e906cf0ede1416b0b776e8886a63762a469c
Rajesh <163125114+Rajesh-D-kasar@users.noreply.github.com>
2026-06-13 01:08:46 -0700

    Update cartApi.js

--- frontend/src/api/cartApi.js
+++ frontend/src/api/cartApi.js
@@ -1,1 +1,10 @@
+import API from "./axiosConfig";
 
+export const addToCart = async (productId, quantity) => {
+  const response = await API.post("/cart/add", {
+    product_id: productId,
+    quantity: quantity,
+  });
+
+  return response.data;
+};
